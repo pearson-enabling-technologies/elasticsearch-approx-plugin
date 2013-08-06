0b027a92f234658008ca273dba02303e22dd15d8
Andrew Clegg <andrew.clegg@pearson.com>
2013-08-07 00:13:44 +0100

    First couple of tests passing, none of the hard stuff done yet though

--- src/main/java/com/pearson/entech/elasticsearch/facet/approx/datehistogram/DistinctDateHistogramFacetParser.java
+++ src/main/java/com/pearson/entech/elasticsearch/facet/approx/datehistogram/DistinctDateHistogramFacetParser.java
@@ -1,5 +1,7 @@
 package com.pearson.entech.elasticsearch.facet.approx.datehistogram;
 
+import java.io.IOException;
+
 import org.elasticsearch.common.collect.ImmutableMap;
 import org.elasticsearch.common.collect.MapBuilder;
 import org.elasticsearch.common.component.AbstractComponent;
@@ -23,8 +25,6 @@ import org.elasticsearch.search.facet.FacetPhaseExecutionException;
 import org.elasticsearch.search.facet.datehistogram.DateHistogramFacet;
 import org.elasticsearch.search.internal.SearchContext;
 
-import java.io.IOException;
-
 /**
  * Parser is responsible to make sense of a SearchRequests "facet query" and
  * has to choose the correct FacetExecutor based on the facet query
@@ -37,10 +37,10 @@ public class DistinctDateHistogramFacetParser extends AbstractComponent implemen
     private final TObjectIntHashMap<String> rounding = new TObjectIntHashMap<String>(Constants.DEFAULT_CAPACITY, Constants.DEFAULT_LOAD_FACTOR, -1);
 
     @Inject
-    public DistinctDateHistogramFacetParser(Settings settings) {
+    public DistinctDateHistogramFacetParser(final Settings settings) {
         super(settings);
 
-        dateFieldParsers = MapBuilder.<String, DateFieldParser>newMapBuilder()
+        dateFieldParsers = MapBuilder.<String, DateFieldParser> newMapBuilder()
                 .put("year", new DateFieldParser.YearOfCentury())
                 .put("1y", new DateFieldParser.YearOfCentury())
                 .put("month", new DateFieldParser.MonthOfYear())
@@ -67,12 +67,11 @@ public class DistinctDateHistogramFacetParser extends AbstractComponent implemen
         rounding.put("halfCeiling", MutableDateTime.ROUND_HALF_CEILING);
     }
 
-
     @Override
     public String[] types() {
-        return new String[]{
-            StringInternalDistinctDateHistogramFacet.TYPE,
-            LongInternalDistinctDateHistogramFacet.TYPE
+        return new String[] {
+                StringInternalDistinctDateHistogramFacet.TYPE,
+                LongInternalDistinctDateHistogramFacet.TYPE
         };
     }
 
@@ -87,7 +86,7 @@ public class DistinctDateHistogramFacetParser extends AbstractComponent implemen
     }
 
     @Override
-    public FacetExecutor parse(String facetName, XContentParser parser, SearchContext context) throws IOException {
+    public FacetExecutor parse(final String facetName, final XContentParser parser, final SearchContext context) throws IOException {
         String keyField = null;
         long interval = 1;
         String sInterval = null;
@@ -96,116 +95,120 @@ public class DistinctDateHistogramFacetParser extends AbstractComponent implemen
         XContentParser.Token token;
         String fieldName = null;
 
-
         String distinctField = null;
-        MutableDateTime dateTime = new MutableDateTime(DateTimeZone.UTC);
+        final MutableDateTime dateTime = new MutableDateTime(DateTimeZone.UTC);
 
         // get the interesting fields from the "facet-query"
         // basically it's the same code as in the regular DateHisogramFacetParser
-        while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
-            if (token == XContentParser.Token.FIELD_NAME) {
+        while((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
+            if(token == XContentParser.Token.FIELD_NAME) {
                 fieldName = parser.currentName();
-            } else if (token == XContentParser.Token.START_OBJECT) {
-            } else if (token.isValue()) {
-                if ("field".equals(fieldName)) {
+            } else if(token == XContentParser.Token.START_OBJECT) {} else if(token.isValue()) {
+                if("field".equals(fieldName)) {
                     keyField = parser.text();
-                } else if ("key_field".equals(fieldName) || "keyField".equals(fieldName)) {
+                } else if("key_field".equals(fieldName) || "keyField".equals(fieldName)) {
                     keyField = parser.text();
-                } else if ("value_field".equals(fieldName) || "distinctField".equals(fieldName)) {
+                } else if("value_field".equals(fieldName) || "distinctField".equals(fieldName)) {
                     distinctField = parser.text();
-                } else if ("interval".equals(fieldName)) {
+                } else if("interval".equals(fieldName)) {
                     intervalSet = true;
-                    if (token == XContentParser.Token.VALUE_NUMBER) {
+                    if(token == XContentParser.Token.VALUE_NUMBER) {
                         interval = parser.longValue();
                     } else {
                         sInterval = parser.text();
                     }
-                } else if ("time_zone".equals(fieldName) || "timeZone".equals(fieldName)) {
-                    if (token == XContentParser.Token.VALUE_NUMBER) {
+                } else if("time_zone".equals(fieldName) || "timeZone".equals(fieldName)) {
+                    if(token == XContentParser.Token.VALUE_NUMBER) {
                         dateTime.setZone(DateTimeZone.forOffsetHours(parser.intValue()));
                     } else {
-                        String text = parser.text();
-                        int index = text.indexOf(':');
-                        if (index != -1) {
+                        final String text = parser.text();
+                        final int index = text.indexOf(':');
+                        if(index != -1) {
                             // format like -02:30
                             dateTime.setZone(DateTimeZone.forOffsetHoursMinutes(
                                     Integer.parseInt(text.substring(0, index)),
                                     Integer.parseInt(text.substring(index + 1))
-                            ));
+                                    ));
                         } else {
                             // id, listed here: http://joda-time.sourceforge.net/timezones.html
                             dateTime.setZone(DateTimeZone.forID(text));
                         }
                     }
-                } else if ("order".equals(fieldName) || "comparator".equals(fieldName)) {
+                } else if("order".equals(fieldName) || "comparator".equals(fieldName)) {
                     comparatorType = DateHistogramFacet.ComparatorType.fromString(parser.text());
                 }
             }
         }
 
         // validation; opposed to the DateHistogramFacetParser the distinctField and interval is also required
-        if (keyField == null) {
-            throw new FacetPhaseExecutionException(facetName, "key field is required to be set for distinct histogram facet, either using [field] or using [key_field]");
+        if(keyField == null) {
+            throw new FacetPhaseExecutionException(facetName,
+                    "key field is required to be set for distinct histogram facet, either using [field] or using [key_field]");
         }
-        FieldMapper keyMapper = context.smartNameFieldMapper(keyField);
-        if (keyMapper == null) {
+        final FieldMapper keyMapper = context.smartNameFieldMapper(keyField);
+        if(keyMapper == null) {
             throw new FacetPhaseExecutionException(facetName, "(key) field [" + keyField + "] not found");
-        } else if (!keyMapper.fieldDataType().getType().equals("long")) {
+        } else if(!keyMapper.fieldDataType().getType().equals("long")) {
             throw new FacetPhaseExecutionException(facetName, "(key) field [" + keyField + "] is not of type date");
         }
-        if (distinctField == null) {
-            throw new FacetPhaseExecutionException(facetName, "distinct field is required to be set for distinct histogram facet, either using [value_field] or using [distinctField]");
+        if(distinctField == null) {
+            throw new FacetPhaseExecutionException(facetName,
+                    "distinct field is required to be set for distinct histogram facet, either using [value_field] or using [distinctField]");
         }
-        FieldMapper distinctFieldMapper = context.smartNameFieldMapper(distinctField);
-        if (distinctFieldMapper == null) {
+        final FieldMapper distinctFieldMapper = context.smartNameFieldMapper(distinctField);
+        if(distinctFieldMapper == null) {
             throw new FacetPhaseExecutionException(facetName, "no mapping found for " + distinctField);
         }
-        if (!intervalSet) {
+        if(!intervalSet) {
             throw new FacetPhaseExecutionException(facetName, "[interval] is required to be set for distinct histogram facet");
         }
 
-
         // this is specific to the "Distinct" DateHistogram. Use a MutableDateTime to take care of the interval and rounding.
         // we set the rounding after we set the zone, for it to take affect
-        if (sInterval != null) {
-            int index = sInterval.indexOf(':');
-            if (index != -1) {
+        if(sInterval != null) {
+            final int index = sInterval.indexOf(':');
+            if(index != -1) {
                 // set with rounding
-                DateFieldParser fieldParser = dateFieldParsers.get(sInterval.substring(0, index));
-                if (fieldParser == null) {
-                    throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval + "] with custom rounding using built in intervals (year/month/...)");
+                final DateFieldParser fieldParser = dateFieldParsers.get(sInterval.substring(0, index));
+                if(fieldParser == null) {
+                    throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval
+                            + "] with custom rounding using built in intervals (year/month/...)");
                 }
-                DateTimeField field = fieldParser.parse(dateTime.getChronology());
-                int rounding = this.rounding.get(sInterval.substring(index + 1));
-                if (rounding == -1) {
-                    throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval + "], rounding type [" + (sInterval.substring(index + 1)) + "] not found");
+                final DateTimeField field = fieldParser.parse(dateTime.getChronology());
+                final int rounding = this.rounding.get(sInterval.substring(index + 1));
+                if(rounding == -1) {
+                    throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval + "], rounding type ["
+                            + (sInterval.substring(index + 1)) + "] not found");
                 }
                 dateTime.setRounding(field, rounding);
             } else {
-                DateFieldParser fieldParser = dateFieldParsers.get(sInterval);
-                if (fieldParser != null) {
-                    DateTimeField field = fieldParser.parse(dateTime.getChronology());
+                final DateFieldParser fieldParser = dateFieldParsers.get(sInterval);
+                if(fieldParser != null) {
+                    final DateTimeField field = fieldParser.parse(dateTime.getChronology());
                     dateTime.setRounding(field, MutableDateTime.ROUND_FLOOR);
                 } else {
                     // time interval
                     try {
                         interval = TimeValue.parseTimeValue(sInterval, null).millis();
-                    } catch (Exception e) {
-                        throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval + "], tried both as built in intervals (year/month/...) and as a time format");
+                    } catch(final Exception e) {
+                        throw new FacetPhaseExecutionException(facetName, "failed to parse interval [" + sInterval
+                                + "], tried both as built in intervals (year/month/...) and as a time format");
                     }
                 }
             }
         }
 
-
-
-        if (distinctFieldMapper.fieldDataType().getType().equals("string")) {
-            PagedBytesIndexFieldData distinctFieldData = context.fieldData().getForField(distinctFieldMapper);
-            LongArrayIndexFieldData keyIndexFieldData = context.fieldData().getForField(keyMapper);
+        // TODO refactor...
+        if(distinctFieldMapper.fieldDataType().getType().equals("string")) {
+            final PagedBytesIndexFieldData distinctFieldData = context.fieldData().getForField(distinctFieldMapper);
+            final LongArrayIndexFieldData keyIndexFieldData = context.fieldData().getForField(keyMapper);
             return new StringDistinctDateHistogramFacetExecutor(keyIndexFieldData, distinctFieldData, dateTime, interval, comparatorType);
-        } else if (distinctFieldMapper.fieldDataType().getType().equals("long"))  {
-            IndexNumericFieldData distinctFieldData = context.fieldData().getForField(distinctFieldMapper);
-            IndexNumericFieldData keyIndexFieldData = context.fieldData().getForField(keyMapper);
+        } else if(distinctFieldMapper.fieldDataType().getType().equals("long")
+                || distinctFieldMapper.fieldDataType().getType().equals("int")
+                || distinctFieldMapper.fieldDataType().getType().equals("short")
+                || distinctFieldMapper.fieldDataType().getType().equals("byte")) {
+            final IndexNumericFieldData distinctFieldData = context.fieldData().getForField(distinctFieldMapper);
+            final IndexNumericFieldData keyIndexFieldData = context.fieldData().getForField(keyMapper);
             return new LongDistinctDateHistogramFacetExecutor(keyIndexFieldData, distinctFieldData, dateTime, interval, comparatorType);
         } else {
             throw new FacetPhaseExecutionException(facetName, "distinct field [" + distinctField + "] is not of type string or long");
@@ -220,49 +223,49 @@ public class DistinctDateHistogramFacetParser extends AbstractComponent implemen
 
         static class WeekOfWeekyear implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.weekOfWeekyear();
             }
         }
 
         static class YearOfCentury implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.yearOfCentury();
             }
         }
 
         static class MonthOfYear implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.monthOfYear();
             }
         }
 
         static class DayOfMonth implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.dayOfMonth();
             }
         }
 
         static class HourOfDay implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.hourOfDay();
             }
         }
 
         static class MinuteOfHour implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.minuteOfHour();
             }
         }
 
         static class SecondOfMinute implements DateFieldParser {
             @Override
-            public DateTimeField parse(Chronology chronology) {
+            public DateTimeField parse(final Chronology chronology) {
                 return chronology.secondOfMinute();
             }
         }
