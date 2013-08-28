62a9ec15dbc3efb0b20dd2404b4a658c732bbce4
Andrew Clegg <andrew.clegg@pearson.com>
2013-08-28 09:56:17 +0100

    Updated name of test

--- src/test/java/com/pearson/entech/elasticsearch/search/facet/approx/datehistogram/MediumDataSetTenThreadsPerformanceTest.java
+++ src/test/java/com/pearson/entech/elasticsearch/search/facet/approx/datehistogram/MediumDataSetTenThreadsPerformanceTest.java
@@ -3,13 +3,13 @@ package com.pearson.entech.elasticsearch.search.facet.approx.datehistogram;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MediumDataSetFiveThreadsPerformanceTest extends MediumDataSetPerformanceTest {
+public class MediumDataSetTenThreadsPerformanceTest extends MediumDataSetPerformanceTest {
 
-    private final ExecutorService _fiveThreads = Executors.newFixedThreadPool(10);
+    private final ExecutorService _tenThreads = Executors.newFixedThreadPool(10);
 
     @Override
     protected ExecutorService threadPool() {
-        return _fiveThreads;
+        return _tenThreads;
     }
 
 }
