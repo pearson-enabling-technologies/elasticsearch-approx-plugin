6cbd7e152a9581a0f2ecdaec9e99ad5a29d2bb35
Andrew Clegg <andrew.clegg@pearson.com>
2013-09-08 16:19:03 +0100

    Special handling of ordinals is no longer helpful - removed

--- src/main/java/com/pearson/entech/elasticsearch/search/facet/approx/date/collectors/BytesFieldIterator.java
+++ src/main/java/com/pearson/entech/elasticsearch/search/facet/approx/date/collectors/BytesFieldIterator.java
@@ -8,7 +8,6 @@ import org.apache.lucene.util.IntsRef;
 import org.elasticsearch.index.fielddata.AtomicFieldData;
 import org.elasticsearch.index.fielddata.BytesValues;
 import org.elasticsearch.index.fielddata.BytesValues.Iter;
-import org.elasticsearch.index.fielddata.BytesValues.WithOrdinals;
 import org.elasticsearch.index.fielddata.IndexFieldData;
 import org.elasticsearch.index.fielddata.ScriptDocValues;
 
@@ -28,12 +27,7 @@ public class BytesFieldIterator<B extends AtomicFieldData<? extends ScriptDocVal
 
     @Override
     public void collect(final int doc) throws IOException {
-        if(_bytesFieldValues instanceof WithOrdinals) {
-            _docOrds = ((WithOrdinals) _bytesFieldValues).ordinals().getOrds(doc);
-            _docOrdPointer = _docOrds.offset;
-        } else {
-            _docIter = _bytesFieldValues.getIter(doc);
-        }
+        _docIter = _bytesFieldValues.getIter(doc);
     }
 
     @Override
@@ -43,28 +37,16 @@ public class BytesFieldIterator<B extends AtomicFieldData<? extends ScriptDocVal
 
     @Override
     public boolean hasNext() {
-        if(_bytesFieldValues instanceof WithOrdinals) {
-            return _docOrdPointer < _docOrds.length;
-        } else {
-            return _docIter.hasNext();
-        }
+        return _docIter.hasNext();
     }
 
     @Override
     public BytesRef next() {
-        if(_bytesFieldValues instanceof WithOrdinals) {
-            final BytesRef unsafe = ((WithOrdinals) _bytesFieldValues).getValueByOrd(_docOrds.ints[_docOrdPointer]);
-            _docOrdPointer++;
-            return unsafe;
-        } else {
-            return _docIter.next();
-        }
+        return _docIter.next();
     }
 
     @Override
     public void postCollection() {
-        _docOrds = null;
-        _docOrdPointer = -1;
         _docIter = null;
     }
 
