803234de2ac1b9281696cedf833b1514d574f8f4
Andrew Clegg <andrew.clegg@pearson.com>
2013-09-08 16:32:31 +0100

    Streamlined this executor and collector somewhat

--- src/main/java/com/pearson/entech/elasticsearch/search/facet/approx/termlist/TermListFacetExecutor.java
+++ src/main/java/com/pearson/entech/elasticsearch/search/facet/approx/termlist/TermListFacetExecutor.java
@@ -3,7 +3,6 @@ package com.pearson.entech.elasticsearch.search.facet.approx.termlist;
 import java.io.IOException;
 
 import org.apache.lucene.index.AtomicReaderContext;
-import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.BytesRefHash;
 import org.elasticsearch.index.fielddata.BytesValues;
 import org.elasticsearch.index.fielddata.BytesValues.Iter;
@@ -13,9 +12,9 @@ import org.elasticsearch.search.facet.InternalFacet;
 
 public class TermListFacetExecutor extends FacetExecutor {
 
-    int _maxPerShard;
-    String _facetName;
-    IndexFieldData<?> _indexFieldData;
+    private final int _maxPerShard;
+    private final String _facetName;
+    private final IndexFieldData<?> _indexFieldData;
 
     BytesRefHash _entries = new BytesRefHash();
 
@@ -32,43 +31,35 @@ public class TermListFacetExecutor extends FacetExecutor {
 
     @Override
     public Collector collector() {
-        return new Collector(_maxPerShard);
+        return new Collector();
     }
 
     final class Collector extends FacetExecutor.Collector {
 
-        private BytesValues values;
-
-        Collector(final int maxPerShard) {
-
-        }
+        private BytesValues _values;
 
         @Override
         public void setNextReader(final AtomicReaderContext context) throws IOException {
-            values = _indexFieldData.load(context).getHashedBytesValues();
-        }
+            if(_entries.size() > _maxPerShard)
+                return;
 
-        protected void onValue(final int docId, final BytesRef value, final int hashCode, final BytesValues values) {
-            _entries.add(value, hashCode);
+            _values = _indexFieldData.load(context).getHashedBytesValues();
         }
 
         @Override
         public void collect(final int docId) throws IOException {
-
             if(_entries.size() > _maxPerShard)
                 return;
 
-            if(values.hasValue(docId)) {
-                final Iter iter = values.getIter(docId);
-                while(iter.hasNext() && _entries.size() < _maxPerShard) {
-                    onValue(docId, iter.next(), iter.hash(), values);
-                }
+            final Iter iter = _values.getIter(docId);
+            while(iter.hasNext() && _entries.size() < _maxPerShard) {
+                _entries.add(iter.next(), iter.hash());
             }
-
         }
 
         @Override
         public void postCollection() {}
+
     }
 
 }
